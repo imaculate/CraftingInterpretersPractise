65037a67962f9884cfdecd7ff29d3b834557a943
Imaculate <imaculatemosha@yahoo.com>
2022-01-26 10:49:51 -0800

    Parsing and emitting expressions upto unary

--- clox/compile.c
+++ clox/compile.c
@@ -1,22 +1,175 @@
 #include <stdio.h>
-#include "scanner.h"
+#include <stdlib.h>
 
-void compile(const char* source) {
-    initScanner(source);
-    int line = -1;
+#include "common.h"
+#include "scanner.h"
+#include "compile.h"
+
+typedef struct 
+{
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
+
+typedef enum {
+    PREC_ONE,
+    PREC_ASSIGNMENT,
+    PREC_OR,
+    PREC_AND,
+    PREC_EQUALITY,
+    PREC_COMPARISON,
+    PREC_TERM,
+    PREC_FACTOR,
+    PREC_UNARY,
+    PREC_CALL,
+    PREC_PRIMARY
+} Precedence;
+
+Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk()
+{
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message)
+{
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error",token->line);
+
+    if (token->type == TOKEN_EOF)
+    {
+        fprintf(stderr, " at end");
+    }
+    else if (token->type != TOKEN_ERROR)
+    {
+        fprintf(stderr, " at '%.*s'",token->length, token->start);
+    }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+static void errorAtCurrent(const char* message)
+{
+    errorAt(&parser.current, message);
+}
+
+static void error(const char* message)
+{
+    errorAt(&parser.previous, message);
+}
+
+static void advance()
+{
+    parser.previous = parser.current;
     for (;;)
     {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        }
-        else
-        {
-            printf("   | ");
-        }
-        printf("%2d '%.*s' \n", token.type, token.length, token.start);
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
 
-        if (token.type == TOKEN_EOF) break;
+        errorAtCurrent(parser.current.start);
     }
+}
+
+static void consume(TokenType type, const char* message)
+{
+    if (parser.current.type == type)
+    {
+        advance();
+        return;
+    }
+    errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte)
+{
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2)
+{
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn()
+{
+    emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value)
+{
+    int constant = addConstant(currentChunk(), value);
+    if (constant > UINT8_MAX)
+    {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value)
+{
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void endCompiler()
+{
+    emitReturn();
+}
+
+static void parsePrecedence(Precedence precedence)
+{
+    
+}
+
+static void expression()
+{
+    parsePrecedence(PREC_ASSIGNMENT);
+}
+
+
+static void grouping()
+{
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression");
+}
+
+static void number()
+{
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary()
+{
+    TokenType operatorType = parser.previous.type;
+
+    parsePrecedence(PREC_UNARY);
+    // expression();
+
+    switch (operatorType)
+    {
+        case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+        default: return;
+    }
+}
+
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+    compilingChunk = chunk;
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+    return !parser.hadError;
 }
