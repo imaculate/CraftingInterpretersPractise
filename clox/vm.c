5be9b1c335a2b91131676b82a150d2d5915c3a23
Imaculate <imaculatemosha@yahoo.com>
2022-03-03 08:53:01 -0800

    Compiling native functions

--- clox/vm.c
+++ clox/vm.c
@@ -1,6 +1,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "common.h"
 #include "compile.h"
@@ -24,6 +25,11 @@
     } while(false)
 
 VM vm;
+
+static Value clockNative(int argCount, Value* args)
+{
+    return NUMBER_VALUE((double)clock()/CLOCKS_PER_SEC);
+}
 
 static void resetStack()
 {
@@ -59,12 +65,23 @@ static void runtimeError(const char* format, ...)
     resetStack();
 }
 
+static void defineNative(const char* name, NativeFn function)
+{
+    push(OBJ_VALUE(copyString(name, (int)strlen(name))));
+    push(OBJ_VALUE(newNative(function)));
+    tableSet(&vm.globals, AS_STRING(vm.stack[0]), vm.stack[1]);
+    pop();
+    pop();
+}
+
 void initVM()
 {
     resetStack();
     vm.objects = NULL;
     initTable(&vm.strings);
     initTable(&vm.globals);
+
+    defineNative("clock", clockNative);
 }
 
 void freeVM()
@@ -130,6 +147,14 @@ static bool callValue(Value callee, int argCount)
         {
             case OBJ_FUNCTION:
                 return call(AS_FUNCTION(callee), argCount);
+            case OBJ_NATIVE:
+            {
+                NativeFn native = AS_NATIVE(callee);
+                Value result = native(argCount, vm.stackTop - argCount);
+                vm.stackTop -= argCount + 1;
+                push(result);
+                return true;
+            }
             default:
                 break;
         }
